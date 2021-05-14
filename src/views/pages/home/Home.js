00fd92d9fb5217512e84f277811700784f6fdd1e
meeedwire <l.jeronimo12345@gmail.com>
2021-05-13 22:08:00 -0300

    Update code

--- src/views/pages/home/Home.js
+++ src/views/pages/home/Home.js
@@ -1,17 +1,43 @@
+import api from '../../../service/api';
+
+async function fetchData() {
+  const { data } = await api.get('random');
+
+  return data;
+}
+
+async function fetchCategories(){
+  const { data } = await api.get('categories');
+
+  return data;
+}
+
 let Home = {
   is_private: false,
 
   render: async () => {
-      let view = `
-          <div>
-            <h1>Home</div>
+    const data = await fetchData();
+    const categories = await fetchCategories()
+
+    console.log(categories.join(''))
+    
+    let view = `
+          <div id="container">
+            <select>
+              ${categories.map(item => `<option class="manu-item" >${item}</option>`).join('')}
+            </select>
+            <img src=${data.icon_url}>
+            <h1>Home</h1>
+            <p>${data.value}</p>
           </div>
       `;
 
-      return view
+    return view;
   },
 
-  after_render: async () => {}
-}
+  after_render: async () => {
+  
+  },
+};
 
 export default Home;
